47eb7a856cc3c7c4cc20239af53fc1792e3b687c
Md. Likhon Mia <likhon15-3916@diu.edu.bd>
2024-09-07 11:55:19 -0700

    added clerk authentication

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,6 +5,8 @@ import { FcGoogle } from "react-icons/fc";
 import bannerImg from '../assets/images/heroimage.png'
 import iconImg from '../assets/icons/icon.png'
 import { Link } from 'react-router-dom';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
     return (
@@ -94,15 +96,23 @@ const Login = () => {
                                 <span className="px-2 bg-white text-gray-500">or</span>
                             </div>
                         </div>
+                        <header className="header">
+                            <header>
+                                <SignedOut>
+                                    <SignInButton />
+                                </SignedOut>
+                                <SignedIn>
+                                    <UserButton />
+                                </SignedIn>
+                            </header>
+                        </header>
                         <div className="mt-6 grid grid-cols-2 gap-3">
                             <div>
-                                <a
-                                    href="#"
+                                <button
                                     className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                                 >
-                                    <FcGoogle className='size-5' />
-                                    Google
-                                </a>
+                                    <FcGoogle className='size-5' />Google
+                                </button>
                             </div>
                             <div>
                                 <a
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,20 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/routes.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </StrictMode>,
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/login">
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </ClerkProvider>
+  </StrictMode>
 )
